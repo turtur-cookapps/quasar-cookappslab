c0127f37a2efb5865b6171df2a0c63d263b6124e
turtur <yjko@cookapps.com>
2018-07-24 19:17:42 +0900

    기본셋팅

---
+++ src/layouts/default-top.vue
@@ -0,0 +1,39 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-layout-header>
+      <q-toolbar
+        color="primary"
+        :glossy="$q.theme === 'mat'"
+        :inverted="$q.theme === 'ios'"
+      >
+        <q-toolbar-title>
+          CookAppsLab
+          <div slot="subtitle">created by team-ninja</div>
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-layout-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { openURL } from 'quasar'
+
+export default {
+  name: 'LayoutDefault',
+  data () {
+    return {
+      leftDrawerOpen: this.$q.platform.is.desktop
+    }
+  },
+  methods: {
+    openURL
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <q-page class="flex flex-center">
+  </q-page>
+</template>
+
+<style>
+</style>
+
+<script>
+export default {
+  name: 'PageIndex',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
