e04a82fe8980dc24bde15e3a465f7d0afe6a0f3d
turtur <yjko@cookapps.com>
2018-07-25 02:18:45 +0900

    로그인 페이지 시안

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,5 +1,17 @@
 <template>
-  <q-page class="flex flex-center">
+  <q-page class="flex flex-center bg">
+    <div class="row">
+      <div class="col-12 text-center board">
+        <h3>Welcome</h3>
+        <h3>CookAppsLab!</h3>
+      </div>
+      <div class="col-12 text-center board">
+        <q-btn color="white" size="lg" text-color="orange-8">Log in with Google</q-btn>
+      </div>
+      <div class="col-12 text-center board">
+        <h6>Log in with email</h6>
+      </div>
+    </div>
   </q-page>
 </template>
 
@@ -8,7 +20,7 @@
 
 <script>
 export default {
-  name: 'PageIndex',
+  name: 'Login',
   data () {
     return {
 
@@ -16,3 +28,17 @@ export default {
   }
 }
 </script>
+
+<style lang="stylus">
+@import '~variables'
+.bg
+  background-color $orange
+.board
+  background-color $orange-7
+.board > h3
+  color white
+  font-weight bold
+.board > h6
+  color rgba(255, 255, 255, 0.5); 
+  font-weight 100
+</style>
