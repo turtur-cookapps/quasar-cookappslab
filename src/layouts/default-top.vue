02e1e78eefc31a00197caf22d5f889bbdf107e09
turtur <yjko@cookapps.com>
2018-07-25 02:18:25 +0900

    탑바 전용 레이아웃 생성

--- src/layouts/default-top.vue
+++ src/layouts/default-top.vue
@@ -1,32 +1,26 @@
 <template>
+  <div>
   <q-layout view="lHh Lpr lFf">
     <q-layout-header>
-      <q-toolbar
-        color="primary"
-        :glossy="$q.theme === 'mat'"
-        :inverted="$q.theme === 'ios'"
-      >
-        <q-toolbar-title>
-          CookAppsLab
-          <div slot="subtitle">created by team-ninja </div>
-        </q-toolbar-title>
+      <q-toolbar>
+        <q-toolbar-title>CookAppsLab</q-toolbar-title>
       </q-toolbar>
     </q-layout-header>
-
     <q-page-container>
       <router-view />
     </q-page-container>
   </q-layout>
+  </div>
 </template>
 
 <script>
 import { openURL } from 'quasar'
 
 export default {
-  name: 'LayoutDefault',
+  name: 'default-top',
   data () {
     return {
-      leftDrawerOpen: this.$q.platform.is.desktop
+      
     }
   },
   methods: {
@@ -34,6 +28,3 @@ export default {
   }
 }
 </script>
-
-<style>
-</style>
