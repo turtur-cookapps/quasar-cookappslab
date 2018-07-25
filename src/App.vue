314e18ad3bcfa0505870aed567d5fa31006c4e70
turtur <yjko@cookapps.com>
2018-07-25 18:31:52 +0900

    파베연결

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,22 @@
 </template>
 
 <script>
+
+import firebase from 'firebase'
+
 export default {
-  name: 'App'
+  name: 'App',
+  created () {
+    var config = {
+      apiKey: 'AIzaSyBnVm-EMwoY6GTOLt7vDx99chywobTrrTg',
+      authDomain: 'turtur-cookappslab.firebaseapp.com',
+      databaseURL: 'https://turtur-cookappslab.firebaseio.com',
+      projectId: 'turtur-cookappslab',
+      storageBucket: 'turtur-cookappslab.appspot.com',
+      messagingSenderId: '118371064312'
+    }
+    firebase.initializeApp(config)
+  }
 }
 </script>
 
