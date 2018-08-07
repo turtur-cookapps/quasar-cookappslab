47ab1d1c2dd7f7acf1426e928c8ad51b41cb2cd2
turtur <yjko@cookapps.com>
2018-08-07 18:55:54 +0900

    컴포넌트 추가

--- quasar.conf.js
+++ quasar.conf.js
@@ -54,7 +54,11 @@ module.exports = function (ctx) {
         'QListHeader',
         'QItem',
         'QItemMain',
-        'QItemSide'
+        'QItemSide',
+        'QTabs',
+        'QTab',
+        'QTabPane',
+        'QRouteTab'
       ],
       directives: [
         'Ripple'
@@ -62,7 +66,8 @@ module.exports = function (ctx) {
       // Quasar plugins
       plugins: [
         'Notify',
-        'Dialog'
+        'Dialog',
+        'Loading'
       ]
       // iconSet: ctx.theme.mat ? 'material-icons' : 'ionicons'
       // i18n: 'de' // Quasar language
