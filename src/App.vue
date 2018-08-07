9c3cd44a98bb9a76406940f84349e121813511d5
turtur <yjko@cookapps.com>
2018-08-07 17:07:30 +0900

    파이어베이스 init 부분을 index로 이관

--- src/App.vue
+++ src/App.vue
@@ -6,21 +6,8 @@
 
 <script>
 
-import firebase from 'firebase'
-
 export default {
-  name: 'App',
-  created () {
-    var config = {
-      apiKey: 'AIzaSyBnVm-EMwoY6GTOLt7vDx99chywobTrrTg',
-      authDomain: 'turtur-cookappslab.firebaseapp.com',
-      databaseURL: 'https://turtur-cookappslab.firebaseio.com',
-      projectId: 'turtur-cookappslab',
-      storageBucket: 'turtur-cookappslab.appspot.com',
-      messagingSenderId: '118371064312'
-    }
-    firebase.initializeApp(config)
-  }
+  name: 'App'
 }
 </script>
 
