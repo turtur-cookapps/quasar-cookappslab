6127bf9537a5df1a3c503e014b36b9076908057b
turtur <yjko@cookapps.com>
2018-08-07 18:56:43 +0900

    상단 탭메뉴 추가

--- src/layouts/default-top.vue
+++ src/layouts/default-top.vue
@@ -1,11 +1,50 @@
 <template>
   <div>
-  <q-layout view="lHh Lpr lFf">
+  <q-layout>
     <q-layout-header>
       <q-toolbar>
-        <q-toolbar-title>CookAppsLab</q-toolbar-title>
-        <q-btn v-if="this.currentUser" dense flat icon="menu" label="LogOut" @click=onClickForlogOut></q-btn>
+        <q-toolbar-title>
+          <q-btn flat dense @onClick="onClickMainIcon">CookAppsLab</q-btn>
+        </q-toolbar-title>
+        <q-btn v-if="this.currentUser" dense flat icon="menu" label="LogOut" @click=onClicklogOut></q-btn>
       </q-toolbar>
+      <q-tabs v-if="this.currentUser" align="left">
+        <q-route-tab
+          icon="face"
+          to="/user"
+          exact
+          slot="title"
+          label="유저관리"
+        />
+        <q-route-tab
+          icon="group"
+          to="/group"
+          exact
+          slot="title"
+          label="그룹관리"
+        />
+        <q-route-tab
+          icon="filter_list"
+          to="/datagen"
+          exact
+          slot="title"
+          label="이벤트생성"
+        />
+        <q-route-tab
+          icon="fast_forward"
+          to="/test"
+          exact
+          slot="title"
+          label="테스트"
+        />
+        <q-route-tab
+          icon="show_chart"
+          to="/chart"
+          exact
+          slot="title"
+          label="챠트"
+        />
+      </q-tabs>
     </q-layout-header>
     <q-page-container>
       <router-view />
@@ -27,13 +66,16 @@ export default {
   },
   methods: {
     openURL,
-    onClickForlogOut () {
+    onClicklogOut () {
       firebase.auth().signOut().then(() => {
         this.$q.dialog({title: 'ㅃㅃ!'}).then(() => {
           this.setCurrentUser(null)
           this.$router.push('/login')
         })
       })
+    },
+    onClickMainIcon () {
+      this.$router.push('/main')
     },
     ...mapMutations('cookappslab', [
       'setCurrentUser'
